bf044813946dbb3e75dbf78623bf2d17c3118fc8
ghfxdcsdga-a11y <ghfxdcsdga@gmail.com>
2026-08-14 22:00:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Подключаемся к Supabase
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_KEY);
